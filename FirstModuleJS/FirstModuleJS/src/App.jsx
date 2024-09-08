076c78b65f9c6596749ff0fb4cf4809c316de123
kojsok <pmbiclub@gmail.com>
2024-09-08 16:30:40 +0300

    add children button

--- FirstModuleJS/FirstModuleJS/src/App.jsx
+++ FirstModuleJS/FirstModuleJS/src/App.jsx
@@ -1,12 +1,41 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import JournalItem from "./components/JournalItem/JournalItem";
+import CardButton from "./components/CardButton/CardButton";
 
 function App() {
+
+  const data = [
+    {
+      title: "Подготовка к обновлению курса",
+      text: "Горные походы открывают удивительные природные ландшафт",
+      date: new Date()
+    },
+    {
+      title: "Поход в горы",
+      text: "Думал, что очень много времени",
+      date: new Date()
+    },
+  ]
+  
+
   return (
     <>
       <h1>Заголовок</h1>
       <p>Какойто текст</p>
       <Button />
+      <CardButton>
+        Новое воспоминание
+      </CardButton>
+      <CardButton> 
+        <JournalItem title={data[0].title} text={data[0].text} date={data[0].date} />
+      </CardButton>
+
+      <CardButton> 
+        <JournalItem title={data[1].title} text={data[1].text} date={data[1].date} />
+      </CardButton>
+      
+
     </>
   );
 }
